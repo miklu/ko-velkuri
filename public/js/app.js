7fddf3efeacdba9ef5d5e7f5b5b10f4f1b7ac86f
miikka <datahanska@gmail.com>
2014-08-15 04:24:52 +0300

    Vedon muokkaus toimii

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,8 @@ var ViewModel = function() {
   var self = this;
   self.velat = ko.observableArray();
   self.velanTiedot = ko.observableArray();
-  self.lomake = {lainaaja: ko.observable(), velallinen: ko.observable(), summa: ko.observable(), kuvaus: ko.observable()};
+  self.lomake = {lainaaja: ko.observable(), velallinen: ko.observable(), summa: ko.observable(), kuvaus: ko.observable(), isNew: ko.observable(true)};
+  self.muokattavaID = null;
 
   self.haeVelat = function() {
     $.get('/velat', function(data) {
@@ -18,16 +19,28 @@ var ViewModel = function() {
       summa: self.lomake.summa(),
       kuvaus: self.lomake.kuvaus(),
     };
+    // Tarkistetaan onko kyseessä uusi velka
+    if(self.lomake.isNew()) {
+      $.post('/tallenna', velka)
+        .done(function() {
+          self.haeVelat();
+          self.tyhjennaLomake();
+      });
+    }
+    // Muokkaus
+    else {
+      self.lomake.isNew(true);
+      $.ajax({
+          type: 'PUT',
+          url: '/muokkaa/' + self.muokattavaID,
+          data: velka,
+          success: function(data) {
+            self.haeVelat();
+            self.tyhjennaLomake();
+          }
+        });
 
-    $.post('/tallenna', velka)
-      .done(function() {
-        self.haeVelat();
-        self.velanTiedot(null);
-        self.lomake.lainaaja(null);
-        self.lomake.velallinen(null);
-        self.lomake.summa(null);
-        self.lomake.kuvaus(null);
-    });
+    }
   };
 
   self.naytaVelanTiedot = function(clicked, elem) {
@@ -47,6 +60,24 @@ var ViewModel = function() {
       });
       self.velanTiedot(null);
       self.haeVelat();
+  };
+
+  self.muokkaa = function(clicked, elem) {
+    console.log('TODO');
+    self.lomake.isNew(false);
+    self.muokattavaID = clicked._id;
+    self.lomake.lainaaja(clicked.lainaaja);
+    self.lomake.velallinen(clicked.velallinen);
+    self.lomake.summa(clicked.summa);
+    self.lomake.kuvaus(clicked.kuvaus);
+  };
+
+  self.tyhjennaLomake = function() {
+    self.velanTiedot(null);
+    self.lomake.lainaaja(null);
+    self.lomake.velallinen(null);
+    self.lomake.summa(null);
+    self.lomake.kuvaus(null);
   };
 
   self.haeVelat();
